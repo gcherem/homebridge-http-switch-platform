35513f38b9ef34f26eb3cae0f20bff48eb4e9595
Gabriel Cherem <gfcherem@gmail.com>
2020-10-15 13:43:18 -0300

    tested

--- src/platform.ts
+++ src/platform.ts
@@ -1,7 +1,9 @@
 import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, Service, Characteristic } from 'homebridge';
+import http, {IncomingMessage, Server, ServerResponse} from 'http';
 
-import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
+import { PLATFORM_NAME, PLUGIN_NAME, DEVICE_COUNT } from './settings';
 import { ExamplePlatformAccessory } from './platformAccessory';
+import * as request from 'request-promise-native';
 
 /**
  * HomebridgePlatform
@@ -9,11 +11,13 @@ import { ExamplePlatformAccessory } from './platformAccessory';
  * parse the user config and discover/register accessories with Homebridge.
  */
 export class HomebridgePlatform implements DynamicPlatformPlugin {
+  private requestServer?: Server;
   public readonly Service: typeof Service = this.api.hap.Service;
   public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
 
   // this is used to track restored cached accessories
   public readonly accessories: PlatformAccessory[] = [];
+
 
   constructor(
     public readonly log: Logger,
@@ -30,6 +34,7 @@ export class HomebridgePlatform implements DynamicPlatformPlugin {
       log.debug('Executed didFinishLaunching callback');
       // run the method to discover / register your devices as accessories
       this.discoverDevices();
+      this.createHttpService();
     });
   }
 
@@ -49,22 +54,71 @@ export class HomebridgePlatform implements DynamicPlatformPlugin {
    * Accessories must only be registered once, previously created accessories
    * must not be registered again to prevent "duplicate UUID" errors.
    */
-  discoverDevices() {
-    for (let i = 0; i < 48; i++) {
+  async discoverDevices() {
+    const status = await this.getStatus();
+    for (let i = 0; i < DEVICE_COUNT; i++) {
       const uniqueId = 'L'+('00' + i).slice(-2);
       const uuid = this.api.hap.uuid.generate(uniqueId);
       const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
       if (existingAccessory) {
         this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
         new ExamplePlatformAccessory(this, existingAccessory);
+        existingAccessory.context.index = i;
+        existingAccessory.context.statusOn = status[i]===1;
         this.api.updatePlatformAccessories([existingAccessory]);
       } else {
         this.log.info('Adding new accessory:', uniqueId);
         const accessory = new this.api.platformAccessory(uniqueId, uuid);
-        accessory.context.device = i;
+        accessory.context.index = i;
+        accessory.context.statusOn = status[i]===1;
         new ExamplePlatformAccessory(this, accessory);
         this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
       }
     }
   }
+
+  async getStatus() {
+    const url = `${this.config.url}get_status`;
+    let response;
+    try {
+      response = await request.get({url});
+    } catch (exception) {
+      this.log.error(`ERROR received from ${url}: ${exception}`);
+    }
+    return JSON.parse(response).st;
+  }
+
+  createHttpService() {
+    // const port = this.config.port || 
+    this.requestServer = http.createServer(this.handleRequest.bind(this));
+    this.requestServer.listen(this.config.localPort, () => this.log.info(`Http server listening on ${this.config.localPort}...`));
+  }
+
+  private handleRequest(request: IncomingMessage, response: ServerResponse) {
+    if (request.url === '/setStatus') {
+      const chucks = [];
+      request
+        .on('data', (chunk) => {
+          chucks.push(chunk as never);
+        })
+        .on('end', () => {
+          const body = Buffer.concat(chucks).toString();
+          this.setStatus(JSON.parse(body).st);
+        });
+    }
+    response.writeHead(204); // 204 No content
+    response.end();
+  }
+
+  setStatus(statuses) {
+    statuses.forEach((status, i) => {
+      const on = status === 1;
+      if (this.accessories[i].context.statusOn !== on) {
+        this.accessories[i].context.statusOn = on;
+        const service = this.accessories[i].getService(this.Service.Lightbulb);
+        (service as Service).updateCharacteristic(this.Characteristic.On, on);
+      }
+    });
+  }
+
 }
