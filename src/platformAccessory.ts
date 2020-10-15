1a15fab308cf8bf945a503cf45043aa0f3340ef3
Gabriel Cherem <gfcherem@gmail.com>
2020-10-14 22:51:07 -0300

    features

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,6 +1,6 @@
 import { Service, PlatformAccessory, CharacteristicValue, CharacteristicSetCallback, CharacteristicGetCallback } from 'homebridge';
 
-import { ExampleHomebridgePlatform } from './platform';
+import { HttpSwitchPlatform } from './platform';
 
 /**
  * Platform Accessory
@@ -20,7 +20,7 @@ export class ExamplePlatformAccessory {
   };
 
   constructor(
-    private readonly platform: ExampleHomebridgePlatform,
+    private readonly platform: HttpSwitchPlatform,
     private readonly accessory: PlatformAccessory,
   ) {
 
